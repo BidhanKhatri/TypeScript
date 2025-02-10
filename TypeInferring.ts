efb5a505bd5f8d59e31554e1a0a3be61ac4b3151
Bidhan Khatri <khatribidhan9@gmail.com>
2025-02-10 16:23:41 +0545

    type inferring

---
+++ TypeInferring.ts
@@ -0,0 +1,13 @@
+// Here we're inferring the types!
+
+let tech = "TypeSctipt";
+let favNumber = 4;
+let isEasy = false;
+
+console.log(tech, favNumber, isEasy);
+
+
+// Error
+// tech = true
+// favNumber = "Hello"
+// isEasy = 14
