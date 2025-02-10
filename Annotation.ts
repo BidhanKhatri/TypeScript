ef714e0d4a8b0b2bd23c9fb2cc410c516173a3a2
Bidhan Khatri <khatribidhan9@gmail.com>
2025-02-10 16:22:27 +0545

    annotation

---
+++ Annotation.ts
@@ -0,0 +1,14 @@
+//String annotaion
+let myName: string = "John";
+//myName = 12 // Error cannot assign a number to a string
+console.log(myName);
+
+//Number annotation
+let num: number = 10;
+//num = "Bidhan" // Error cannot assign a string to a number
+console.log(num);
+
+//Boolean annotation
+let isHard:Boolean = false
+//isHard = 12 // Error cannot assign a number to a boolean
+console.log(isHard);
